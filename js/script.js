dfcf5334ad5fbaa4d53b604445957867790dc9c9
Jordan Hoover <jordan@hoovermld.com>
2019-06-20 22:49:41 -0500

    Add quote objects and script to show random quotes.

--- js/script.js
+++ js/script.js
@@ -3,45 +3,114 @@
  project 1 - A Random Quote Generator
  ******************************************/
 
-// Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
+/**
+ * @typedef Quote
+ * @type {Object}
+ *
+ * @property {string} quote - The text of a quote.
+ * @property {string} source - The origin or source of the quote.
+ * @property {string} citation - (optional) The identifying venue of the quote. For example, a speech, publication, movie.
+ * @property {string} year - (optional) A number identifying the year of the quote.
+ * @property {string[]} tags - (optional) The categories of each quote.
+ */
+let quotes = [
+    {
+        quote: 'Talk is cheap. Show me the code.',
+        source: 'Linus Torvalds',
+        citation: '',
+        year: '',
+        tags: ['progamming']
+    },
+    {
+        quote: 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+        source: 'Martin Fowler',
+        citation: '',
+        year: '',
+        tags: ['refactoring']
+    },
+    {
+        quote: 'A primary cause of complexity is that software vendors uncritically adopt almost any feature that users want.',
+        source: 'Niklaus Wirth',
+        citation: 'Twitter',
+        year: '',
+        tags: ['users', 'design']
+    },
+    {
+        quote: 'Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live',
+        source: 'John Woods',
+        citation: '',
+        year: '',
+        tags: ['code', 'progamming']
+    },
+    {
+        quote: 'Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the Universe trying to produce bigger and better idiots. So far, the Universe is winning.',
+        source: 'Rick Cook',
+        citation: 'The Wizardry Compiled',
+        year: '1990',
+        tags: ['programming', 'books']
+    },
+    {
+        quote: 'Truth can only be found in one place: the code.',
+        source: 'Robert C. Martin',
+        citation: 'Clean Code: A Handbook of Agile Software Craftsmanship',
+        year: '2008',
+        tags: ['code', 'books']
+    },
+];
 
+/**
+ * Return a random quote object from the quotes array.
+ *
+ * @returns {Quote}
+ */
+const getRandomQuote = () => {
+    return quotes[getRandomNumber(quotes.length)];
+};
 
-/***
- Create the array of quote objects and name it `quotes`.
- Add at least five quote objects to the `quotes` array.
- Give each quote object a `quote` and `source` property.
- Add the `citation` property to at least one object in the array.
- Add the `year` property to at least one object in the array.
- Use console.log() to log your array of quotes to the console.
- ***/
+const getRandomNumber = (max) => {
+    return Math.floor(Math.random() * Math.floor(max));
+};
 
+/**
+ * Creates quote html and places in quote box.
+ */
+const printQuote = () => {
+    let randomQuote = getRandomQuote();
+    let html = '';
 
+    html += `<p class="quote">${randomQuote.quote}</p>`;
+    html += `<p class="source">${randomQuote.source}`;
 
+    if (randomQuote.citation) {
+        html += `<span class="citation">${randomQuote.citation}</span>`;
+    }
 
-/***
-  Create the `getRandomQuote` function to:
-   - Create a variable to store a random number
-   - Cse the random number to `return` a random quote object from the `quotes` array.
-***/
+    if (randomQuote.year) {
+        html += `<span class="year">${randomQuote.year}</span>`;
+    }
+    html += '</p>';
 
+    if (randomQuote.tags) {
+        html += `<p class="tags">Tags: ${randomQuote.tags.join(', ')}</p>`;
+    }
 
+    document.getElementById('quote-box').innerHTML = html;
+    updateBackgroundColor();
+};
 
+const updateBackgroundColor = () => {
+    const colors = ['blue', 'tomato', 'violet', 'evergreen', 'orange'];
 
-/***
-  Create the `printQuote` function to:
-   - Call the `getRandomQuote` function and assign it to a variable.
-   - Create a variable for the HTML string and set it equal to an empty string.
-   - Use the HTML template in the instructions or the markup in the index.html file, AND
-     the random quote vairable to build your HTML string.
-   - Add the quote and source section to the HTML string.
- - Use an if statement to check for the citation property before adding it to the HTML string.
- - Use an if statement to check for the year property before adding it to the HTML string.
- - Don't forget to close that final `p` tag.
- - Set the `innerHTML` of the `quote-box` div to the HTML string.
- ***/
+    let newColor = colors[getRandomNumber(colors.length)];
 
+    let prevColor = document.body.style.background;
 
+    while (prevColor === newColor) {
+        newColor = colors[getRandomNumber(colors.length)];
+    }
 
+    document.body.style.backgroundColor = newColor;
+};
 
 /***
  When the "Show another quote" button is clicked, the event listener
@@ -52,5 +121,12 @@
 
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+// 30 seconds
+const intervalTime = 30000;
 
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+setInterval(
+    function () {
+        printQuote();
+    },
+    intervalTime
+);
